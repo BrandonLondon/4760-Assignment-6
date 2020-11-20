61be96cbfd9f6933699a3709ddec9860b19fa877
Brandon London <55200147+BrandonLondon@users.noreply.github.com>
2020-11-19 21:26:57 -0600

    Add files via upload

---
+++ Assignment6/queue.h
@@ -0,0 +1,34 @@
+/* ====================================================================================================
+# Author: Brandon London
+# Course: CS4760
+# File Name: queue.h
+==================================================================================================== */
+#ifndef MY_QUEUE_H
+#define MY_QUEUE_H
+
+
+typedef struct NodeQ
+{ 
+	int index;
+	struct NodeQ *next;
+}QNode; 
+
+
+typedef struct
+{ 
+	QNode *front;
+	QNode *rear;
+	int count;
+}Queue; 
+
+
+Queue *createQueue();
+QNode *newQNode(int index);
+void enQueue(Queue* q, int index);
+QNode *deQueue(Queue *q);
+bool isQueueEmpty(Queue *q);
+int getQueueCount(Queue *q);
+char *getQueue(const Queue *q);
+
+#endif
+
